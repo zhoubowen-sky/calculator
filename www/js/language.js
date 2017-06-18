182d6488035f9b2349ab9ee0df0d3ab63f954b57
Zhoubowen <1784225410@qq.com>
2017-06-19 00:14:48 +0800

    添加多语言配置文件

---
+++ www/js/language.js
@@ -0,0 +1,21 @@
+// 此文件为APP的多语言配置文件
+
+var Language_ZH = {
+
+    doubleClickToExit: "双击退出应用程序",
+    appName: "灵动计算器",
+    btnOK: "确定",
+    btnCancle: "取消"
+
+}
+
+var Language_EN = {
+
+    doubleClickToExit: "Double click to exit"
+
+}
+
+module.exports = {
+    Language_ZH: Language_ZH,
+    Language_EN: Language_EN
+}
